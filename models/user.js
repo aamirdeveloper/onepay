e5b692a94b628788919504dea23c5588476a2365
aamirdeveloper <aamir.0019@gmail.com>
2024-07-31 15:55:44 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -14,11 +14,16 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     referenceId: DataTypes.STRING,
     loginCode: DataTypes.STRING,
     loggedIn: DataTypes.STRING,
+    totalDeposit: DataTypes.DOUBLE,
+    totalWithdraw: DataTypes.DOUBLE,
+    balance: DataTypes.DOUBLE,
   }, {
     sequelize,
     modelName: 'User',
