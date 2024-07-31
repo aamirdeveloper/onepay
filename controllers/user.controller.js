1f1232eabe3e4213b909294e26ed858775ec29d6
aamirdeveloper <aamir.0019@gmail.com>
2024-07-31 15:56:59 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -17,11 +17,30 @@ function index(req, res){
         let userId = user.userId;
         
         models.User.findOne({where:{id:userId}}).then(result =>{
+            console.log(result);
+            let totalTransaction = 0;
+            models.Transaction.findAll({
+                where: {
+                    userId: userId
+                },
+            }).then(result1 => {
 
-            res.status(200).json({
-                status: 1,
-                data: result,
-                totalBets: totalBets
+                totalTransaction = result1.length;
+
+                var arr = {
+                    status: 1, 
+                    "message": "success",
+                    "userId": result.id,
+                    "firstName": result.firstName,
+                    "lastName": result.lastName,
+                    "email": result.email,
+                    "totalDeposit": result.totalDeposit,
+                    "totalWithdraw": result.totalWithdraw,
+                    "balance": result.balance,
+                    "totalTransaction": totalTransaction
+                };
+
+                res.status(200).json(arr);
             });
             
         }).catch(error => {
