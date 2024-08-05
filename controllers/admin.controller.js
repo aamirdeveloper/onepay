2cb520a0e9f2f3d94fd436314e8dc256dab70420
aamirdeveloper <aamir.0019@gmail.com>
2024-08-05 15:59:17 +0530

    Add files via upload

---
+++ controllers/admin.controller.js
@@ -0,0 +1,285 @@
+const Validator = require('fastest-validator');
+const models = require('../models');
+const jwt = require('jsonwebtoken'); 
+const helper = require('../helper');
+const bycryptjs = require("bcryptjs");
+require('dotenv').config();
+
+//Login function
+function login(req, res){
+    const post = {
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    const schema = {
+        email: {type: "string", optional: false, empty: false},
+        password: {type: "string", optional: false, empty: false}
+    }
+
+    const v = new Validator();
+    const validationResponse = v.validate(post, schema);
+
+    if(validationResponse !== true){
+        return res.status(200).json({
+            status: 0,
+            message: "validation failed",
+            errors: validationResponse
+        });
+    }
+
+    models.Admin.findOne({where:{email:req.body.email}}).then(result =>{
+        if(result === null){
+            res.status(200).json({
+                status: 0,
+                message: "Invalid Credentials"
+            });
+        }else{
+            bycryptjs.compare(req.body.password, result.password, function(err, results){
+                if(results){
+                    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+                    let data = {
+                        time: Date(),
+                        email: result.email,
+                        adminId: result.id
+                    }
+                    const token = jwt.sign(data, jwtSecretKey);
+                    var arr = {
+                        status: 1, 
+                        "message": "login successfull",
+                        "adminId": result.id,
+                        "email": result.email,
+                        "name": result.name,
+                        "token": token
+                    };
+
+                    res.status(200).json(arr);
+                }else{
+                    res.status(200).json({
+                        status: 0,
+                        message: "Invalid Credentials"
+                    });
+                }
+            });
+        }
+    }).catch(error => {
+        res.status(200).json({
+            status: 2,
+            message: "Something went wrong"
+        });
+    });
+}
+
+function change_password(req, res) {
+    let resp = helper.check_token(req);
+    if(resp !== "Successfully Verified")
+    {
+        console.error(`Token error`, resp);
+        res.json(resp);
+    }
+    else
+    {
+        const post = {
+            currentPassword: req.body.currentPassword,
+            newPassword: req.body.newPassword
+        }
+
+        const schema = {
+            currentPassword: {type: "string", optional: false},
+            newPassword: {type: "string", optional: false}
+        }
+
+        const v = new Validator();
+        const validationResponse = v.validate(post, schema);
+
+        if(validationResponse !== true){
+            return res.status(200).json({
+                status: 0,
+                message: "validation failed",
+                errors: validationResponse
+            });
+        }
+
+        let login_data = helper.get_adminId_token(req);
+        const adminId = login_data.adminId;
+
+        models.Admin.findOne({where:{id:adminId}}).then(result =>{
+            if(result === null){
+                res.status(200).json({
+                    status: 0,
+                    message: "Invalid token"
+                });
+            }else{
+                bycryptjs.compare(req.body.currentPassword, result.password, function(err, results){
+                    if(results){
+                        bycryptjs.genSalt(10, function(err, salt){
+                            bycryptjs.hash(req.body.newPassword, salt, function(err, hash){
+                                const dt = {
+                                    password: hash
+                                }
+                            
+                                models.Admin.update(dt, {where:{
+                                    id:adminId
+                                }}).then(result => {
+                                    res.status(200).json({
+                                        status: 1,
+                                        message: "Password updated"
+                                    });
+                                }).catch(error => {
+                                    res.status(200).json({
+                                        status: 0,
+                                        message: "Something went wrong!"
+                                    });
+                                });
+                            });
+                        });
+                    }else{
+                        res.status(200).json({
+                            status: 0,
+                            message: "Current password is incorrect"
+                        });
+                    }
+                });
+            }
+        }).catch(error => {
+            res.status(200).json({
+                status: 2,
+                message: "Something went wrong"
+            });
+        });
+    }
+}
+
+function all_transactions(req, res){
+    let resp = helper.check_token(req);
+    if(resp !== "Successfully Verified")
+    {
+        console.error(`Token error`, resp);
+        res.json(resp);
+    }
+    else
+    {
+        models.Transaction.findAll().then(result => {
+            res.status(200).json({
+                status: 1,
+                data: result
+            });
+        }).catch(error => {
+            res.status(200).json({
+                status: 2,
+                message: "Something went wrong!",
+                error: error
+            });
+        });
+    }
+}
+
+function all_users(req, res){
+    let resp = helper.check_token(req);
+    if(resp !== "Successfully Verified")
+    {
+        console.error(`Token error`, resp);
+        res.json(resp);
+    }
+    else
+    {
+        models.User.findAll({
+            attributes: [
+               'id', 'firstName', 'lastName', 'email', 'referenceId', 'totalDeposit', 'totalWithdraw', 'balance', 'createdAt', 'updatedAt'
+            ],
+        }).then(result => {
+            res.status(200).json({
+                status: 1,
+                data: result
+            });
+        }).catch(error => {
+            res.status(200).json({
+                status: 2,
+                message: "Something went wrong!",
+                error: error
+            });
+        });
+    }
+}
+
+function users_transactions(req, res){
+    let resp = helper.check_token(req);
+    if(resp !== "Successfully Verified")
+    {
+        console.error(`Token error`, resp);
+        res.json(resp);
+    }
+    else
+    {
+        const post = {
+            userId: req.body.userId
+        }
+
+        const schema = {
+            userId: {type: "string", optional: false, empty: false}
+        }
+
+        const v = new Validator();
+        const validationResponse = v.validate(post, schema);
+
+        if(validationResponse !== true){
+            return res.status(200).json({
+                status: 0,
+                message: "validation failed",
+                errors: validationResponse
+            });
+        }
+        
+        let userId = post.userId;
+
+        models.Transaction.findAll({
+            where: {
+                userId: userId
+            },
+        }).then(result => {
+            res.status(200).json({
+                status: 1,
+                data: result
+            });
+        }).catch(error => {
+            res.status(200).json({
+                status: 2,
+                message: "Something went wrong!",
+                error: error
+            });
+        });
+    }
+}
+
+function contact_requests(req, res){
+    let resp = helper.check_token(req);
+    if(resp !== "Successfully Verified")
+    {
+        console.error(`Token error`, resp);
+        res.json(resp);
+    }
+    else
+    {
+        models.ContactRequest.findAll().then(result => {
+            res.status(200).json({
+                status: 1,
+                data: result
+            });
+        }).catch(error => {
+            res.status(200).json({
+                status: 2,
+                message: "Something went wrong!",
+                error: error
+            });
+        });
+    }
+}
+
+module.exports = {
+    login: login,
+    change_password: change_password,
+    all_transactions:all_transactions,
+    all_users:all_users,
+    users_transactions:users_transactions,
+    contact_requests:contact_requests
+};
