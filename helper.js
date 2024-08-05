269a7ebd75fa58ee5260628be422cd8306826e92
aamirdeveloper <aamir.0019@gmail.com>
2024-08-05 16:32:45 +0530

    Update helper.js

--- helper.js
+++ helper.js
@@ -49,7 +49,34 @@ function get_userId_token(req) {
     }
 }
 
+function get_adminId_token(req) {
+    const jwt = require('jsonwebtoken');
+    require('dotenv').config();
+    
+    let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  
+    try {
+        const token = req.header(tokenHeaderKey);
+  
+        const verified = jwt.verify(token, jwtSecretKey);
+        if(verified)
+        {
+            const token_data = jwt.decode(token, jwtSecretKey);
+            return token_data;
+        }
+        else{
+            // Access Denied
+            return error;
+        }
+    } catch (error) {
+        // Access Denied
+        return error;
+    }
+}
+
 module.exports = {
   check_token,
-  get_userId_token
+  get_userId_token,
+  get_adminId_token
 }
