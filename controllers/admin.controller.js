8de497f3a5680f697739ec03bfd34715789f3c21
aamirdeveloper <aamir.0019@gmail.com>
2024-08-14 20:28:36 +0530

    Update admin.controller.js

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -5,6 +5,19 @@ const helper = require('../helper');
 const bycryptjs = require("bcryptjs");
 require('dotenv').config();
 
+const fs = require('fs');
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads');
+      },
+    filename: function (req, file, cb) {
+        cb(null, Date.now()+file.originalname);
+    }
+});
+const uploadImg = multer({storage: storage}).single('image');
+
 function index(req, res){
     let resp = helper.check_token(req);
     if(resp !== "Successfully Verified")
