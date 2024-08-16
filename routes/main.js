bf1cb97a38415410b4aa07caf7c1352cdab6a605
aamirdeveloper <aamir.0019@gmail.com>
2024-08-16 19:01:03 +0530

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.post('/save-contact', mainController.save_contact)
 
+router.post('/payment-link-details', mainController.payment_link_details)
+
 module.exports= router;
