2f341f97ccc129d7705161075efd6509b4618488
aamirdeveloper <aamir.0019@gmail.com>
2024-09-02 20:06:30 +0530

    Add files via upload

---
+++ models/withdrawRequest.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class WithdrawRequest extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  WithdrawRequest.init({
+    userId: DataTypes.INTEGER,
+    withdrawType: DataTypes.STRING,
+    withdrawMethod: DataTypes.STRING,
+    amount: DataTypes.DOUBLE,
+    bankName: DataTypes.STRING,
+    bankAccountNumber: DataTypes.STRING,
+    bankAccountName: DataTypes.STRING,
+    currency: DataTypes.STRING,
+    timePeriod: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'WithdrawRequest',
+  });
+  return WithdrawRequest;
+};
