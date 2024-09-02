f67b9d883a87019832df24c11208ec758e6b666c
aamirdeveloper <aamir.0019@gmail.com>
2024-09-02 17:53:27 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -26,34 +26,74 @@ function index(req, res){
     {
         let user = helper.get_userId_token(req);
         let userId = user.userId;
-        
+
+        let bankDepositFess = 0;
+        let totalTransaction = 0;
+        let user_data = [];
+
         models.User.findOne({where:{id:userId}}).then(result =>{
-            console.log(result);
-            let totalTransaction = 0;
+            user_data = result;
+            
             models.Transaction.findAll({
                 where: {
                     userId: userId
                 },
             }).then(result1 => {
-
                 totalTransaction = result1.length;
 
-                var arr = {
-                    status: 1, 
-                    "message": "success",
-                    "userId": result.id,
-                    "firstName": result.firstName,
-                    "lastName": result.lastName,
-                    "email": result.email,
-                    "totalDeposit": result.totalDeposit,
-                    "totalWithdraw": result.totalWithdraw,
-                    "balance": result.balance,
-                    "totalTransaction": totalTransaction
-                };
+                models.UsersBank.findOne({
+                    where: {
+                        userId:userId
+                    },
+                    order: [
+                        ['id', 'DESC']
+                    ]
+                }).then(result1 => {
+                    bankDepositFess = result1.fees;
 
-                res.status(200).json(arr);
+                    models.usersCryptoAccount.findOne({
+                        where: {
+                            userId:userId
+                        },
+                        order: [
+                            ['id', 'DESC']
+                        ]
+                    }).then(result2 => {
+                        let cryptoDepositFess = result2.fees;
+
+                        var arr = {
+                            status: 1, 
+                            "message": "success",
+                            "userId": user_data.id,
+                            "firstName": user_data.firstName,
+                            "lastName": user_data.lastName,
+                            "email": user_data.email,
+                            "totalDeposit": user_data.totalDeposit,
+                            "totalWithdraw": user_data.totalWithdraw,
+                            "balance": user_data.balance,
+                            "totalTransaction": totalTransaction,
+                            "bankDepositFess": bankDepositFess,
+                            "cryptoDepositFess": cryptoDepositFess
+                        };
+
+                        res.status(200).json(arr);
+                    }).catch(error => {
+                        res.status(200).json({
+                            status: 2,
+                            message: "Something went wrong!",
+                            error: error
+                        });
+                    });
+                }).catch(error => {
+                    res.status(200).json({
+                        status: 2,
+                        message: "Something went wrong!",
+                        error: error
+                    });
+                });
+                
             });
-            
+
         }).catch(error => {
             res.status(200).json({
                 status: 2,
