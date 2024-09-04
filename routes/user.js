84907cb2bb44aba2f2d8b2241c2a9160ebfeb866
aamirdeveloper <aamir.0019@gmail.com>
2024-09-04 13:54:03 +0530

    Add files via upload

--- routes/user.js
+++ routes/user.js
@@ -14,6 +14,7 @@ router.post('/change-password', loginController.change_password)
 router.get('/dashboard', userController.index);
 
 router.post('/add-payment-link', userController.uploadImg, userController.add_payment_link);
+router.get('/all-payment-links', userController.all_payment_links);
 router.post('/delete-payment-link', userController.delete_payment_link);
 
 router.get('/transaction-deposits', userController.transaction_deposits);
